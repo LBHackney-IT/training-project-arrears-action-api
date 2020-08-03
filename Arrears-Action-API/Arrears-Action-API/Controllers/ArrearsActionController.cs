f5412e54d57b58d307a874a6448160b8f5e2df4c
Liudvikas Taluntis <liudvikast@gmail.com>
2020-08-03 16:04:20 +0100

    Initial commit!
    .Net Core 2.1 AWS Lambda API template with S3 stuff removed, because it's unneeded.
    
    Added a custom dummy controller to check whether the application works - the further development will continue from there.
    
    There's this web.config from a template. Can't tell if it's something important or not for the lambda. I'll leave it as I won't use it to store any sensitive information anyway - there're environment variables for that.

---
+++ Arrears-Action-API/Arrears-Action-API/Controllers/ArrearsActionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arrears_Action_API.Controllers
+{
+    [Route("api/v1/ArrearsActions")]
+    [ApiController]
+    public class ArrearsActionsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Hello/Name/{name}")]
+        public IActionResult HelloSomeone([FromRoute] string name)
+        {
+            return Ok($"It's working?! Uhm... Hello, {name}!");
+        }
+    }
+}
